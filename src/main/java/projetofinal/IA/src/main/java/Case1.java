706f7cd5d0d454e8cfaa174652c7397cf855dbc2
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-29 00:56:21 -0300

    feat: drawTimetable

--- src/main/java/projetofinal/IA/src/main/java/Case1.java
+++ src/main/java/projetofinal/IA/src/main/java/Case1.java
@@ -7,42 +7,144 @@ import aima.core.search.csp.*;
 
 public class Case1 extends CSP<Variable, String>{
 	
-	// Definição de variáveis
-	public static Variable seg1 = new Variable("2M1B1");
-	public static Variable seg2 = new Variable("2M1B2");
-	public static Variable seg3 = new Variable("2M2B1");
-	public static Variable seg4 = new Variable("2M2B2");
-	public static Variable seg5 = new Variable("2M3B1");
-	public static Variable seg6 = new Variable("2M3B2");
+	/* Definição de variáveis */
+	public static Variable H2T1 = new Variable("H2T1");
+	public static Variable H2T2 = new Variable("H2T2");
+	public static Variable H2T3 = new Variable("H2T3");
+	public static Variable H2T4 = new Variable("H2T4");
+	public static Variable H2T5 = new Variable("H2T5");
+	public static Variable H2T6 = new Variable("H2T6");
+
+	public static Variable H3T1 = new Variable("H3T1");
+	public static Variable H3T2 = new Variable("H3T2");
+	public static Variable H3T3 = new Variable("H3T3");
+	public static Variable H3T4 = new Variable("H3T4");
+	public static Variable H3T5 = new Variable("H3T5");
+	public static Variable H3T6 = new Variable("H3T6");
+
+	public static Variable H4T1 = new Variable("H4T1");
+	public static Variable H4T2 = new Variable("H4T2");
+	public static Variable H4T3 = new Variable("H4T3");
+	public static Variable H4T4 = new Variable("H4T4");
+	public static Variable H4T5 = new Variable("H4T5");
+	public static Variable H4T6 = new Variable("H4T6");
+
+	public static Variable H5T1 = new Variable("H5T1");
+	public static Variable H5T2 = new Variable("H5T2");
+	public static Variable H5T3 = new Variable("H5T3");
+	public static Variable H5T4 = new Variable("H5T4");
+	public static Variable H5T5 = new Variable("H5T5");
+	public static Variable H5T6 = new Variable("H5T6");
+
+	public static Variable H6T1 = new Variable("H6T1");
+	public static Variable H6T2 = new Variable("H6T2");
+	public static Variable H6T3 = new Variable("H6T3");
+	public static Variable H6T4 = new Variable("H6T4");
+	public static Variable H6T5 = new Variable("H6T5");
+	public static Variable H6T6 = new Variable("H6T6");
+	
+	public static Variable H2N1 = new Variable("H2N1");
+	public static Variable H2N2 = new Variable("H2N2");
+	public static Variable H2N3 = new Variable("H2N3");
+	public static Variable H2N4 = new Variable("H2N4");
+	
+	public static Variable H3N1 = new Variable("H3N1");
+	public static Variable H3N2 = new Variable("H3N2");
+	public static Variable H3N3 = new Variable("H3N3");
+	public static Variable H3N4 = new Variable("H3N4");
+	
+	public static Variable H4N1 = new Variable("H4N1");
+	public static Variable H4N2 = new Variable("H4N2");
+	public static Variable H4N3 = new Variable("H4N3");
+	public static Variable H4N4 = new Variable("H4N4");
+	
+	public static Variable H5N1 = new Variable("H5N1");
+	public static Variable H5N2 = new Variable("H5N2");
+	public static Variable H5N3 = new Variable("H5N3");
+	public static Variable H5N4 = new Variable("H5N4");
+	
+	public static Variable H6N1 = new Variable("H6N1");
+	public static Variable H6N2 = new Variable("H6N2");
+	public static Variable H6N3 = new Variable("H6N3");
+	public static Variable H6N4 = new Variable("H6N4");
 	
 	// Colocando todas as variáveis em uma Lista
-	static List<Variable> allVar = Arrays.asList(seg1, seg2, seg3, seg4, seg5);
+	static List<Variable> allVar = Arrays.asList( 
+			H2T1, H2T2, H2T3, H2T4, H2T5, H2T6, 
+			H3T1, H3T2, H3T3, H3T4, H3T5, H3T6, 
+			H4T1, H4T2, H4T3, H4T4, H4T5, H4T6, 
+			H5T1, H5T2, H5T3, H5T4, H5T5, H5T6,
+			H6T1, H6T2, H6T3, H6T4, H6T5, H6T6, 
+			H2N1, H2N2, H2N3, H2N4, 
+			H3N1, H3N2, H3N3, H3N4, 
+			H4N1, H4N2, H4N3, H4N4, 
+			H5N1, H5N2, H5N3, H5N4, 
+			H6N1, H6N2, H6N3, H6N4
+	);
 	
-	// Definição das constantes que serão o domínio
+	/* Definição das constantes que serão o domínio */ 
 	public static String COMP0455 = new String("COMP0455");
 	public static String COMP0481 = new String("COMP0481");
 	public static String COMP0408 = new String("COMP0408");
-	public static String VAZIO = new String("VAZIO");
+	public static String PIBIC = new String("PIBIC");
+	public static String VAZIO = new String("  -  ");
 	
+	/* Faz PIBIC e tem 3 disciplinas */
 	public Case1() {
 		// Criação das variáveis
 		super(allVar);
 		
 		Domain<String> atividades = new Domain<>(
-				COMP0408, COMP0455, COMP0481, VAZIO, 
+				COMP0408, COMP0455, COMP0481, PIBIC, VAZIO, 
+				VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO,
+				VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO,
 				VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO
 		);
 		
 		for (Variable var : getVariables())
 			setDomain(var, atividades);
 		
-		// Lista com domínio sem o espaço "em branco"
-		List<String> efectiveDomain = Arrays.asList(COMP0408, COMP0455, COMP0481);
+		//// Lista com domínio sem o espaço "em branco"
+		//List<String> efectiveDomain = Arrays.asList(COMP0408, COMP0455, COMP0481, PIBIC);
 		
-		// Definir restrições
-//		addConstraint(new UnaryConstraint(seg1, COMP0481));
-		addConstraint(new UnaryConstraint(seg2, COMP0455));
-		addConstraint(new DifAllConstraint(allVar));
-		addConstraint(new AllActivitiesConstraint(allVar, efectiveDomain));
+		/* RESTRIÇÕES */
+		// Restrições Unárias
+		addConstraint(new UnaryConstraint(H3T3, COMP0455));
+		addConstraint(new UnaryConstraint(H3T4, COMP0455));
+		addConstraint(new UnaryConstraint(H5T3, COMP0455));
+		addConstraint(new UnaryConstraint(H5T4, COMP0455));
+		addConstraint(new UnaryConstraint(H5N1, COMP0481));
+		addConstraint(new UnaryConstraint(H5N2, COMP0481));
+		addConstraint(new UnaryConstraint(H2N3, COMP0408));
+		addConstraint(new UnaryConstraint(H2N4, COMP0408));
+		addConstraint(new UnaryConstraint(H4N3, COMP0408));
+		addConstraint(new UnaryConstraint(H4N4, COMP0408));
+		addConstraint(new UnaryConstraint(H2T1, PIBIC));
+		addConstraint(new UnaryConstraint(H2T2, PIBIC));
+		addConstraint(new UnaryConstraint(H2T3, PIBIC));
+		addConstraint(new UnaryConstraint(H2T4, PIBIC));
+		addConstraint(new UnaryConstraint(H2T5, PIBIC));
+		addConstraint(new UnaryConstraint(H2T6, PIBIC));
+		addConstraint(new UnaryConstraint(H4T1, PIBIC));
+		addConstraint(new UnaryConstraint(H4T2, PIBIC));
+		addConstraint(new UnaryConstraint(H4T3, PIBIC));
+		addConstraint(new UnaryConstraint(H4T4, PIBIC));
+		addConstraint(new UnaryConstraint(H4T5, PIBIC));
+		addConstraint(new UnaryConstraint(H4T6, PIBIC));
+		addConstraint(new UnaryConstraint(H6T1, PIBIC));
+		addConstraint(new UnaryConstraint(H6T2, PIBIC));
+		addConstraint(new UnaryConstraint(H6T3, PIBIC));
+		addConstraint(new UnaryConstraint(H6T4, PIBIC));
+		addConstraint(new UnaryConstraint(H6T5, PIBIC));
+		addConstraint(new UnaryConstraint(H6T6, PIBIC));
+		addConstraint(new UnaryConstraint(H5T5, PIBIC));
+		addConstraint(new UnaryConstraint(H5T6, PIBIC));
+		
+		
+		// APAGAR ESSAS DAQUI DE BAIXO E USAR DE VOLUME
+		// Restrição de serem todos diferentes
+		// addConstraint(new DifAllConstraint(allVar));
+		// Restrição de todas as atividades estarem realmente na solução (não adicionar mais vazio que necessário)
+		// addConstraint(new AllActivitiesConstraint(allVar, efectiveDomain));
 	}
 }
--- src/main/java/projetofinal/IA/src/main/java/Main.java
+++ src/main/java/projetofinal/IA/src/main/java/Main.java
@@ -1,30 +1,273 @@
 package projetofinal.IA.src.main.java;
 
+import java.util.List;
 import java.util.Optional;
 
 import aima.core.search.csp.Assignment;
 import aima.core.search.csp.CSP;
 import aima.core.search.csp.Variable;
-import aima.core.search.csp.solver.CspHeuristics;
 import aima.core.search.csp.solver.CspListener;
 import aima.core.search.csp.solver.CspSolver;
-import aima.core.search.csp.solver.FlexibleBacktrackingSolver;
 import aima.core.search.csp.solver.MinConflictsSolver;
 
 public class Main {
 
+	public static void drawTimetable(Assignment<Variable, String> solution) {
+		
+		String H2T1 = " - ";
+		String H3T1 = " - ";
+		String H4T1 = " - ";
+		String H5T1 = " - ";
+		String H6T1 = " - ";
+		String H2T2 = " - ";
+		String H3T2 = " - ";
+		String H4T2 = " - ";
+		String H5T2 = " - ";
+		String H6T2 = " - ";
+		String H2T3 = " - ";
+		String H3T3 = " - ";
+		String H4T3 = " - ";
+		String H5T3 = " - ";
+		String H6T3 = " - ";
+		String H2T4 = " - ";
+		String H3T4 = " - ";
+		String H4T4 = " - ";
+		String H5T4 = " - ";
+		String H6T4 = " - ";
+		String H2T5 = " - ";
+		String H3T5 = " - ";
+		String H4T5 = " - ";
+		String H5T5 = " - ";
+		String H6T5 = " - ";
+		String H2T6 = " - ";
+		String H3T6 = " - ";
+		String H4T6 = " - ";
+		String H5T6 = " - ";
+		String H6T6 = " - ";
+		String H2N1 = " - ";
+		String H2N2 = " - ";
+		String H2N3 = " - ";
+		String H2N4 = " - ";
+		String H3N1 = " - ";
+		String H3N2 = " - ";
+		String H3N3 = " - ";
+		String H3N4 = " - ";
+		String H4N1 = " - ";
+		String H4N2 = " - ";
+		String H4N3 = " - ";
+		String H4N4 = " - ";
+		String H5N1 = " - ";
+		String H5N2 = " - ";
+		String H5N3 = " - ";
+		String H5N4 = " - ";
+		String H6N1 = " - ";
+		String H6N2 = " - ";
+		String H6N3 = " - ";
+		String H6N4 = " - ";
+		
+		List<Variable> variables = solution.getVariables();
+		
+		for (Variable variable : variables) {
+			String varName = variable.getName();
+			String varValue = solution.getValue(variable);
+
+			switch(varName) {
+				case "H2T1": 
+				  H2T1 = varValue; break;
+				case "H3T1": 
+				  H3T1 = varValue; break;
+				case "H4T1": 
+				  H4T1 = varValue; break;
+				case "H5T1": 
+				  H5T1 = varValue; break;
+				case "H6T1": 
+				  H6T1 = varValue; break;
+				case "H2T2": 
+				  H2T2 = varValue; break;
+				case "H3T2": 
+				  H3T2 = varValue; break;
+				case "H4T2": 
+				  H4T2 = varValue; break;
+				case "H5T2": 
+				  H5T2 = varValue; break;
+				case "H6T2": 
+				  H6T2 = varValue; break;
+				case "H2T3": 
+				  H2T3 = varValue; break;
+				case "H3T3": 
+				  H3T3 = varValue; break;
+				case "H4T3": 
+				  H4T3 = varValue; break;
+				case "H5T3": 
+				  H5T3 = varValue; break;
+				case "H6T3": 
+				  H6T3 = varValue; break;
+				case "H2T4": 
+				  H2T4 = varValue; break;
+				case "H3T4": 
+				  H3T4 = varValue; break;
+				case "H4T4": 
+				  H4T4 = varValue; break;
+				case "H5T4": 
+				  H5T4 = varValue; break;
+				case "H6T4": 
+				  H6T4 = varValue; break;
+				case "H2T5": 
+				  H2T5 = varValue; break;
+				case "H3T5": 
+				  H3T5 = varValue; break;
+				case "H4T5": 
+				  H4T5 = varValue; break;
+				case "H5T5": 
+				  H5T5 = varValue; break;
+				case "H6T5": 
+				  H6T5 = varValue; break;
+				case "H2T6": 
+				  H2T6 = varValue; break;
+				case "H3T6": 
+				  H3T6 = varValue; break;
+				case "H4T6": 
+				  H4T6 = varValue; break;
+				case "H5T6": 
+				  H5T6 = varValue; break;
+				case "H6T6": 
+				  H6T6 = varValue; break;
+				case "H2N1": 
+					  H2N1 = varValue; break;
+				case "H2N2": 
+				  H2N2 = varValue; break;
+				case "H2N3": 
+				  H2N3 = varValue; break;
+				case "H2N4": 
+				  H2N4 = varValue; break;
+				case "H3N1": 
+				  H3N1 = varValue; break;
+				case "H3N2": 
+				  H3N2 = varValue; break;
+				case "H3N3": 
+				  H3N3 = varValue; break;
+				case "H3N4": 
+				  H3N4 = varValue; break;
+				case "H4N1": 
+				  H4N1 = varValue; break;
+				case "H4N2": 
+				  H4N2 = varValue; break;
+				case "H4N3": 
+				  H4N3 = varValue; break;
+				case "H4N4": 
+				  H4N4 = varValue; break;
+				case "H5N1": 
+				  H5N1 = varValue; break;
+				case "H5N2": 
+				  H5N2 = varValue; break;
+				case "H5N3": 
+				  H5N3 = varValue; break;
+				case "H5N4": 
+				  H5N4 = varValue; break;
+				case "H6N1": 
+				  H6N1 = varValue; break;
+				case "H6N2": 
+				  H6N2 = varValue; break;
+				case "H6N3": 
+				  H6N3 = varValue; break;
+				case "H6N4": 
+				  H6N4 = varValue; break;
+				default: 
+					System.out.println("Problema ao printar. ");
+			}
+		}
+		StringBuilder timetable = new StringBuilder();
+		
+		timetable.append("   Horários    | Segunda |  Terça  |  Quarta  |  Quinta  |  Sexta  |  Sábado  \n");
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T1); timetable.append(" | ");
+		timetable.append(H3T1); timetable.append(" | ");
+		timetable.append(H4T1); timetable.append(" | ");
+		timetable.append(H5T1); timetable.append(" | ");
+		timetable.append(H6T1); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T2); timetable.append(" | ");
+		timetable.append(H3T2); timetable.append(" | ");
+		timetable.append(H4T2); timetable.append(" | ");
+		timetable.append(H5T2); timetable.append(" | ");
+		timetable.append(H6T2); timetable.append("\n");
+		
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T3); timetable.append(" | ");
+		timetable.append(H3T3); timetable.append(" | ");
+		timetable.append(H4T3); timetable.append(" | ");
+		timetable.append(H5T3); timetable.append(" | ");
+		timetable.append(H6T3); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T4); timetable.append(" | ");
+		timetable.append(H3T4); timetable.append(" | ");
+		timetable.append(H4T4); timetable.append(" | ");
+		timetable.append(H5T4); timetable.append(" | ");
+		timetable.append(H6T4); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T5); timetable.append(" | ");
+		timetable.append(H3T5); timetable.append(" | ");
+		timetable.append(H4T5); timetable.append(" | ");
+		timetable.append(H5T5); timetable.append(" | ");
+		timetable.append(H6T5); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2T6); timetable.append(" | ");
+		timetable.append(H3T6); timetable.append(" | ");
+		timetable.append(H4T6); timetable.append(" | ");
+		timetable.append(H5T6); timetable.append(" | ");
+		timetable.append(H6T6); timetable.append("\n");
+		
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2N1); timetable.append(" | ");
+		timetable.append(H3N1); timetable.append(" | ");
+		timetable.append(H4N1); timetable.append(" | ");
+		timetable.append(H5N1); timetable.append(" | ");
+		timetable.append(H6N1); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2N2); timetable.append(" | ");
+		timetable.append(H3N2); timetable.append(" | ");
+		timetable.append(H4N2); timetable.append(" | ");
+		timetable.append(H5N2); timetable.append(" | ");
+		timetable.append(H6N2); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2N3); timetable.append(" | ");
+		timetable.append(H3N3); timetable.append(" | ");
+		timetable.append(H4N3); timetable.append(" | ");
+		timetable.append(H5N3); timetable.append(" | ");
+		timetable.append(H6N3); timetable.append("\n");
+
+		timetable.append("13:00 - 14:00 "); timetable.append(" | ");
+		timetable.append(H2N4); timetable.append(" | ");
+		timetable.append(H3N4); timetable.append(" | ");
+		timetable.append(H4N4); timetable.append(" | ");
+		timetable.append(H5N4); timetable.append(" | ");
+		timetable.append(H6N4); timetable.append("\n");
+		
+		System.out.println(timetable);
+	}
+	
 	public static void main(String[] args) {
 		CSP<Variable, String> csp = new Case1();
 		CspListener.StepCounter<Variable, String> stepCounter = new CspListener.StepCounter<>();
 		CspSolver<Variable, String> solver;
 		Optional<Assignment<Variable, String>> solution;
 		
-		solver = new MinConflictsSolver<>(50);
+		solver = new MinConflictsSolver<>(1000);
 		solver.addCspListener(stepCounter);
 		stepCounter.reset();
 		System.out.println("Classes scheduling (Minimum Conflicts)");
 		solution = solver.solve(csp);
 		solution.ifPresent(System.out::println);
 		System.out.println(stepCounter.getResults() + "\n");
+		
+		if(!solution.isEmpty()) {
+			drawTimetable(solution.get());
+		}
 	}
 }
